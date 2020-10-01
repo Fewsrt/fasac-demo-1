520f01be6ffb3f6e4c3de71a939d4d5704700178
FaSAC-Auto <chonnaphat.v@gmail.com>
2020-10-02 02:31:22 +0700

    up

--- src/views/Control.vue
+++ src/views/Control.vue
@@ -13,64 +13,100 @@
     <h3>Pump pH</h3>
     <v-btn class="ma-2" color="error" outlined @click="light_4on">On</v-btn>
     <v-btn class="ma-2" outlined color="indigo" @click="light_4off">Off</v-btn>
-    <h3>Check Status : {{ relay1 }}</h3>
-    <h3>Check Status : {{ relay2 }}</h3>
-    <h3>Check Status : {{ relay3 }}</h3>
-    <h3>Check Status : {{ relay4 }}</h3>
+    <h2>Relay Status</h2>
+    <li>Relay 1 Status : {{ relay1 }}</li>
+    <li>Relay 2 Status : {{ relay2 }}</li>
+    <li>Relay 3 Status : {{ relay3 }}</li>
+    <li>Relay 4 Status : {{ relay4 }}</li>
+    <h2>Raspi 3</h2>
+    <li>Serial Status : {{ serial_raspi3 }}</li>
+    <li>Temp Status : {{ temp_raspi3 }}</li>
+    <li>Raspi Status : {{ reboot_system }}</li>
+    <v-btn class="ma-2" outlined color="error" @click="reboot">reboot</v-btn>
+    <v-btn class="ma-2" outlined color="error" @click="shutdown">shutdown</v-btn>
+    <h2>Raspi Zero</h2>
+      <li>Serial Status : {{ serial_zero }}</li>
+      <li>Temp Status : {{ temp_zero }}</li>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "control",
   data() {
     return {
-      relay1: "",
-      relay2: "",
-      relay3: "",
-      relay4: ""
+      relay1: this.relay1,
+      relay2: this.relay2,
+      relay3: this.relay3,
+      relay4: this.relay4,
+      reboot_system: this.reboot_system,
+      temp_raspi3: this.temp_raspi3,
+      temp_zero: this.temp_zero,
+      serial_zero: this.serial_zero,
+      serial_raspi3: this.serial_raspi3
     };
   },
   mqtt: {
     "relay-1-on": function(val) {
       var relay1 = new TextDecoder("utf-8").decode(val);
       this.relay1 = val;
-      console.log(relay1);
     },
     "relay-1-off": function(val) {
       var relay1 = new TextDecoder("utf-8").decode(val);
       this.relay1 = val;
-      console.log(relay1);
     },
     "relay-2-on": function(val) {
       var relay2 = new TextDecoder("utf-8").decode(val);
       this.relay2 = val;
-      console.log(relay2);
     },
     "relay-2-off": function(val) {
       var relay2 = new TextDecoder("utf-8").decode(val);
       this.relay2 = val;
-      console.log(relay2);
     },
     "relay-3-on": function(val) {
       var relay3 = new TextDecoder("utf-8").decode(val);
       this.relay3 = val;
-      console.log(relay3);
     },
     "relay-3-off": function(val) {
       var relay3 = new TextDecoder("utf-8").decode(val);
       this.relay3 = val;
-      console.log(relay3);
     },
     "relay-4-on": function(val) {
       var relay4 = new TextDecoder("utf-8").decode(val);
       this.relay4 = val;
-      console.log(relay4);
     },
     "relay-4-off": function(val) {
       var relay4 = new TextDecoder("utf-8").decode(val);
       this.relay4 = val;
-      console.log(relay4);
+    },
+    "Home/RPI3/Temp": function(val) {
+      var temp_raspi3 = new TextDecoder("utf-8").decode(val);
+      this.temp_raspi3 = val;
+    },
+    "reboot/status/reboot": function(val) {
+      var reboot_system = new TextDecoder("utf-8").decode(val);
+      this.reboot_system = val;
+    },
+    "reboot/status/shutdown": function(val) {
+      var reboot_system = new TextDecoder("utf-8").decode(val);
+      this.reboot_system = val;
+    },
+    "reboot/status/connect": function(val) {
+      var reboot_system = new TextDecoder("utf-8").decode(val);
+      this.reboot_system = val;
+    },
+    "Home/RPI3/serial": function(val) {
+      var serial_raspi3 = new TextDecoder("utf-8").decode(val);
+      this.serial_raspi3 = val;
+    },
+    "Home/RPIZero/serial": function(val) {
+      var serial_zero = new TextDecoder("utf-8").decode(val);
+      this.serial_zero = val;
+    },
+    "Home/RPIZero/Temp": function(val) {
+      var temp_zero = new TextDecoder("utf-8").decode(val);
+      this.temp_zero = val;
     }
   },
   created() {
@@ -83,6 +119,13 @@ export default {
     this.$mqtt.subscribe("relay-3-off", { qos: 0 });
     this.$mqtt.subscribe("relay-4-on", { qos: 0 });
     this.$mqtt.subscribe("relay-4-off", { qos: 0 });
+    this.$mqtt.subscribe("Home/RPI3/serial", { qos: 0 });
+    this.$mqtt.subscribe("Home/RPI3/Temp", { qos: 0 });
+    this.$mqtt.subscribe("Home/RPIZero/serial", { qos: 0 });
+    this.$mqtt.subscribe("Home/RPIZero/Temp", { qos: 0 });
+    this.$mqtt.subscribe("reboot/status/reboot", { qos: 0 });
+    this.$mqtt.subscribe("reboot/status/shutdown", { qos: 0 });
+    this.$mqtt.subscribe("reboot/status/connect", { qos: 0 });
   },
   methods: {
     light_1on() {
@@ -108,6 +151,12 @@ export default {
     },
     light_4off() {
       this.$mqtt.publish("raspi/1", "relay-4-0");
+    },
+    reboot() {
+      this.$mqtt.publish("raspi/1", "reboot");
+    },
+    shutdown() {
+      this.$mqtt.publish("raspi/1", "shutdown");
     }
     // onMsg(topic, data) {
     //   console.log("topic", topic);
